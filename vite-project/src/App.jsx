eac6f1ba5fa94b349c5402f85917bf31e0c1c201
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-10 13:56:06 -0300

    feat: creating the about section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,15 @@ import Header from "./Component/Header/Header-component";
 import './Assets/index.css'
 import  './Assets/global.css'
 import Main from "./Component/Main/Main-component";
+import About from "./Component/About/About-component";
 
 function App() {
 
   return (
     <>
       <Header />
-      <Main/>
+      <Main />
+      <About/>
     </>
   )
 }
