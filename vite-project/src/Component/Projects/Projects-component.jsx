7855e0c9f09852b941ef8733c0604a318bf1248c
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-12 16:43:44 -0300

    feat: adding style and component structure

---
+++ vite-project/src/Component/Projects/Projects-component.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './Projects-style.css';
+import DataAnalytics from './projects-categories/DataAnalytics';
+import MyApps from './projects-categories/MyApp';
+import Others from './projects-categories/Others';
+import AllProjecs from './projects-categories/AllProjecs';
+
+// Adicionando a lista de elementos quando estado ser ativado
+const elements = [
+    {
+        label: 'Todos',
+        element: <AllProjecs/>
+    },
+    {
+        label: 'Web App',
+        element: <MyApps/>
+    },
+    {
+        label: 'DataAnalysis',
+        element: <DataAnalytics/>
+    },
+    {
+        label: 'Outros',
+        element: <Others/>
+    },
+];
+
+function Projects() {
+    //
+    const [elementActive, setElementActive] = useState(elements[0].element); // serve para navegar entre elementos
+    const [activeButton, setActiveButton] = useState(elements[0].label); // Estado para acompanhar, através da label, qual botão está ativo - serve para definir background
+
+    return (
+        <section className='section-projects'>
+            <div className='section-projects-content'>
+                <div className='section-project-texts'>
+                    <h1 className='section-project-title'><span>Meu</span><br /> Portfólio</h1>
+                    <p className='section-project-subtitle'>Estes são alguns dos trabalhos e <br/>  projetos que já realizei</p>
+                </div>
+                <div className='section-projects-btn'>
+                    {elements.map(e => (
+                        <button
+                            className={`projects-btn ${activeButton === e.label ? 'active' : ''}`} // Adicionando classe 'active' se o botão estiver ativo
+                            type='button'
+                            key={e.label} // chave do elemento
+                            onClick={() => {
+                                setElementActive(e.element); // Ele vai redenrizar o elemento clicado
+                                setActiveButton(e.label); // Atualizar o estado do botão ativo quando clicado
+                            }}
+                        >
+                            {e.label}
+                        </button>
+                    ))}
+                </div>
+                {/* Aqui é onde aparece os elementos */}
+                <article className='projects-images'>
+                    {elementActive}
+                </article>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
