d602594f7361e215d540b5b053ce1ca153387d15
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-14 02:29:45 -0300

    feat: adding responsiveness to page components

--- vite-project/src/Component/Main/Main-component.jsx
+++ vite-project/src/Component/Main/Main-component.jsx
@@ -9,24 +9,26 @@ function Main() {
             <article className='main-content'>
                 <div className='main-wrapper'>
                     <div className='main-texts'>
-                        <p className='main-title-p'>Olá pessoas, </p>
-                        <h1 className='main-title'>EU SOU UM <br /> PROGRAMADOR</h1>
-                        <p className='main-title-p'>Seja bem-vindo ao meu portifólio website</p>
+                        <div className='main-titles'>
+                            <p className='main-title-p'>Olá pessoas, <br /></p>
+                            <h1 className='main-title'>EU SOU UM <br /> PROGRAMADOR</h1>
+                            <p className='main-title-p'>Seja bem-vindo ao meu portifólio website<br /></p>
+                        </div>
 
                         <button className='main-btn'>
                             Saiba mais sobre mim
                         </button>
 
                         <div className='main-icons'>
-                            <a href="">
+                            <a href="https://github.com/LucasPedro123">
                                 <i  class="fa-brands fa-github fa-3x"></i>
 
                             </a>
-                            <a href="">
+                            <a href="https://www.linkedin.com/in/lucas-pedro-fernandes/">
                                 <i class="fa-brands fa-linkedin-in fa-3x"></i>  
                             </a>
-                            <a href="">
-                                <i class="fa-brands fa-instagram fa-4x"></i>  
+                            <a href="https://www.instagram.com/lucasfernandes4150/">
+                                <i class="fa-brands fa-instagram fa-3x"></i>  
                             </a>
                         </div>
                     </div>
--- vite-project/src/Component/Projects/projects-categories/AllProjecs.jsx
+++ vite-project/src/Component/Projects/projects-categories/AllProjecs.jsx
@@ -9,26 +9,32 @@ import './all-style.css'
 
 const projects = [
   {
+    link: "https://previsao-do-temp.netlify.app/",
     name: "Clima agora",
     img: weatherNowProjectImg
   },
   {
+    link: "https://le-scone-gourmet.netlify.app/",
     name: "Le Scone",
     img: lesconeProjectImg
   },
   {
+    link: "https://app.powerbi.com/view?r=eyJrIjoiYTJmOWMyNGUtYzU4Mi00NGNkLWJmYmYtOGFjMDY0MjVjZmI5IiwidCI6ImIxMDUxYzRiLTNiOTQtNDFhYi05NDQxLWU3M2E3MjM0MmZkZCJ9",
     name: "Dashboard Power BI",
     img: dashboardProjectImg
   },
   {
+    link: "https://65af1bb012f1c381d79ac15e--e-commercetestproject.netlify.app/",
     name: "Ecommerce",
     img: ecommerceProjectImg
   },
   {
+    link: "https://brazilexpress.netlify.app/",
     name: "Brasil Express",
     img: brasilexpressProjectImg
   },
   {
+    link: "https://deburguer-shop.netlify.app/",
     name: "Deburguer Shop",
     img: deburguerProjectImg
   },
@@ -39,7 +45,7 @@ function AllProjects() {
         <div className='img-projects'>
           {
             projects.map(project => (
-              <img key={project.name} src={project.img} alt={project.name} />
+              <img width="100%" key={project.name} src={project.img} alt={project.name} />
             ))
           }
         </div>
